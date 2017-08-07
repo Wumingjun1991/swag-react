d8709a1b6df87dd6ae7b2d93276b41a05de79b4a
wangyukai04 <417929177@qq.com>
2017-08-08 00:12:10 +0800

    修复跨域请求bug

--- app/container/index/index.js
+++ app/container/index/index.js
@@ -1,9 +1,35 @@
 import React from "react";
+import {ajax} from '../../util/index';
 
 export default class extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            src:''
+        }
+    }
+    componentWillMount(){
+        ajax({
+            url:'http://localhost:8333/messages',
+            method:'post',
+            data:{
+                id:1,
+                loged:true,
+            }
+        }).then((data)=>{
+            this.setState({
+                src:data.data.avatar
+            })
+            console.log(data);
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
     render(){
         return (
-            <div>123123123</div>
+            <div>123123123
+                <img src={this.state.src} alt=""/>
+            </div>
         )
     }
 }
