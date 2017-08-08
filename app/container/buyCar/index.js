c50028c470251860163d5384efa29da34f9005c5
suracheng <sura.cheng@outlook.com>
2017-08-08 10:52:03 +0800

    buyCar

---
+++ app/container/buyCar/index.js
@@ -0,0 +1,89 @@
+/**
+ * Created by sura.cheng on 2017/8/8.
+ */
+
+
+
+import React, {Component} from 'react';
+import { NavBar, Icon } from 'antd-mobile';
+import { Card, WingBlank, WhiteSpace } from 'antd-mobile';
+import { List, Checkbox, Flex } from 'antd-mobile';
+
+const CheckboxItem = Checkbox.CheckboxItem;
+const AgreeItem = Checkbox.AgreeItem;
+
+
+
+export default class App extends Component {
+
+
+
+    onChange = () => {
+        console.log("😄");
+    };
+
+    render () {
+
+        const data = [
+            { value: 0, label: '' }
+        ];
+
+        return (
+            <div>
+                {/* nav */}
+                <NavBar leftContent="back"
+                        mode="light"
+                        onLeftClick={() => console.log('onLeftClick')}
+                        rightContent={[
+                            <Icon key="0" type="search" style={{ marginRight: '0.32rem' }} />,
+                            <Icon key="1" type="ellipsis" />,
+                        ]}
+                >购物车</NavBar>
+
+
+                {/* list */}
+                <WingBlank size="lg">
+                    <Card>
+                        <Card.Header title="哈哈哈 我是 title"/>
+                        <Card.Body>
+                            <List>
+                                {data.map(i => (
+                                    <CheckboxItem key={i.value} onChange={() => this.onChange(i.value)}>
+                                        {i.label}
+                                        <a href="#"><img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502169458847&di=04ceb7df91a29dd5fcdecf1d642be352&imgtype=0&src=http%3A%2F%2Fpic15.nipic.com%2F20110616%2F7177713_105928197391_2.jpg" width="162" height="162"/></a>
+                                        <div>
+                                            <span>描述文案</span>
+                                            <p>$money</p>
+                                        </div>
+
+                                    </CheckboxItem>
+                                ))}
+                            </List>
+                        </Card.Body>
+                    </Card>
+                </WingBlank>
+
+
+
+                <List>
+                    <CheckboxItem onChange={() => this.onChange()}>
+
+
+
+                    </CheckboxItem>
+                </List>
+
+
+            </div>
+        )
+    }
+}
+
+import "../../style/buyCar.css";
+
+
+
+
+
+
+
--- app/index.js
+++ app/index.js
@@ -4,12 +4,14 @@ import {HashRouter as Router,Route,Switch} from "react-router-dom";
 
 // 引入进来的东西后面备注上自己名字缩写
 import Index from './container/index/index'; // wyk
+import BuyCar from './container/buyCar/index'; // 购物车
 
 ReactDOM.render(
     <Router>
         <div>
             <Switch>
                 <Route exact path="/" component={Index}/>
+                <Route path="/buyCar" component={BuyCar}/>
             </Switch>
         </div>
     </Router>,
