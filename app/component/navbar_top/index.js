0cdc083b3192f5cd4b9bb58606d8385f0cf9054d
wangyukai04 <417929177@qq.com>
2017-08-08 17:21:14 +0800

    ha

--- app/component/navbar_top/index.js
+++ app/component/navbar_top/index.js
@@ -9,7 +9,9 @@ export default class NavbarTop extends React.Component{
                 <div style={{position:'fixed',top:'0',zIndex:'10',width:'100%'}}>
                     {this.props.children}
                 </div>
-                <WhiteSpace size="lg" /><WhiteSpace size="lg" /><WhiteSpace size="lg" />
+                <WhiteSpace size="lg"/>
+                <WhiteSpace size="lg"/>
+                <WhiteSpace size="lg"/>
             </div>
         )
     }
