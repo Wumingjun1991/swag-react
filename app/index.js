cadb784e1124f3eb64bd0c0d2f29c0a86beb7ef7
Wumingjun1991 <499567890@qq.com>
2017-08-08 12:53:18 +0800

    footer

--- app/index.js
+++ app/index.js
@@ -4,13 +4,19 @@ import {HashRouter as Router,Route,Switch} from "react-router-dom";
 
 // 引入进来的东西后面备注上自己名字缩写
 import Index from './container/index/index'; // wyk
-
+import Footer from './component/footer/index'
+function Message() {
+    return <h1>message</h1>
+}
 ReactDOM.render(
     <Router>
         <div>
             <Switch>
                 <Route exact path="/" component={Index}/>
+                <Route path='/index' component={Index}/>
+                <Route path='/message' component={Message}/>
             </Switch>
+            <Footer/>
         </div>
     </Router>,
     document.querySelector("#app")
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ let path = require('path');
 
 const svgSpriteDirs = [
     require.resolve('antd-mobile').replace(/warn\.js$/, ''), // antd-mobile 内置svg
-    //path.resolve(__dirname, 'src/my-project-svg-foler'),  // 业务代码本地私有 svg 存放目录
+    path.resolve(__dirname, 'app/public/icon'),  // 业务代码本地私有 svg 存放目录
 ];
 
 module.exports = {
