f207646c11394b8f0796933ae7e23c6a433d60af
gepeng820 <496636462@qq.com>
2017-08-08 10:50:07 +0800

    message

--- app/container/message/message.js
+++ app/container/message/message.js
@@ -1,7 +1,17 @@
 import React,{Component} from 'react';
 import {ajax} from '../../util/index.js';
+import { NavBar, Icon } from 'antd-mobile';
+import { List } from 'antd-mobile';
+import './message.less';
+
+const Item = List.Item;
+const Brief = Item.Brief;
+
 
 export default class extends Component{
+    state = {
+    disabled: false,
+  }
 
 
     componentWillMount(){
@@ -25,7 +35,35 @@ export default class extends Component{
 
     render(){
         return(
-            <div>1111</div>
+        <div className='message'>
+        {/*导航   */}
+        <NavBar mode="light" className="navbar"
+        >消息</NavBar>   
+
+
+        <List renderHeader={() => 'Subtitle'} className="my-list">
+        <Item arrow="horizontal" multipleLine>
+          Title <Brief>subtitle</Brief>
+        </Item>
+        <Item
+          arrow="horizontal"
+          multipleLine
+          onClick={() => {}}
+          platform="android"
+        >
+          ListItem （Android）<Brief>There may have water ripple effect of <br /> material if you set the click event.</Brief>
+        </Item>
+        <Item
+          arrow="horizontal"
+          thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
+          multipleLine
+          onClick={() => {}}
+        >
+          Title <Brief>subtitle</Brief>
+        </Item>
+        </List>
+        </div>
+            
         )
     }
 }
--- app/index.js
+++ app/index.js
@@ -8,23 +8,14 @@ import Index from './container/index/index'; // wyk
 import Message from './container/message/message'; // gp
 
 ReactDOM.render(
-    <div>
-        <Router>
-        <div>
-            <Switch>
-                <Route exact path="/" component={Index}/>
-            </Switch>
-        </div>
-    </Router>
     <Router>
         <div>
             <Switch>
-                <Route exact path="/messages" component={Message}/>
+                <Route exact path="/" component={Index}/>
+                <Route path="/message" component={Message}/>
             </Switch>
         </div>
     </Router>
-    </div>
-    
     ,
     document.querySelector("#app")
 );
