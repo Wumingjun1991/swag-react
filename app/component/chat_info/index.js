00c776956443776122d1632371b66d63b5aba090
Wumingjun1991 <499567890@qq.com>
2017-08-10 00:56:16 +0800

    聊天页面

---
+++ app/component/chat_info/index.js
@@ -0,0 +1,70 @@
+/**
+ * Created by w4995 on 2017/8/9.
+ */
+import React, {Component} from 'react'
+import {ajax} from '../../util/index'
+import NavbarTop from '../../component/navbar_top/index'
+import {NavBar, Icon,TextareaItem} from 'antd-mobile'
+import TalkPopover from '../talk_popover/index'
+import ip from '../../util/ipLocation';
+import './index.less'
+export default class ChatInfo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            disabled: false,
+            code: 0,
+            data: {
+                avatar: '',
+                name: '',
+                username: '',
+                sellername: '',
+                selleravatar: '',
+
+            }
+        };
+    }
+
+    componentWillMount() {
+        ajax({
+            method: 'GET',
+            url: `http://${ip}:8333/userinfo`
+        }).then((res) => {
+            console.log(res);
+            this.setState(res)
+        })
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <NavbarTop>
+                    <NavBar leftContent="back"
+                            iconName="left"
+                            rightContent="设置"
+                            mode="light">{this.state.sellername}</NavBar>
+                </NavbarTop>
+                <TalkPopover user={{
+                    avatar: this.state.data.avatar,
+                    user: 'buy'
+                }}/>
+                <TalkPopover user={{
+                    avatar: this.state.data.avatar,
+                    user: 'seller'
+                }}/>
+                <TalkPopover user={{
+                    avatar: this.state.data.avatar,
+                    user: 'buy'
+                }}/>
+                <TalkPopover user={{
+                    avatar:this.state.data.avatar,
+                    user:'buy'
+                }}/>
+                <div className="user-input">
+                    <TextareaItem placeholder="输入内容" autoHeight="true"></TextareaItem>
+                </div>
+            </div>
+        )
+    }
+}
