899410e194d4a87314d39c8da4abd0f71d4001ad
Wumingjun1991 <499567890@qq.com>
2017-08-10 17:50:22 +0800

    聊天升级

--- app/component/talk_popover/index.js
+++ app/component/talk_popover/index.js
@@ -4,24 +4,12 @@
 import React, {Component} from 'react'
 import './index.less'
 export default class TalkPopover extends Component {
-    constructor() {
-        super();
-        this.state = {
-            user: 'buy',
-            avatar: '',
-            name: '',
-            username: '',
-            content: '',
-            time: '',
-        }
-    }
-
     render() {
         return (
             <div className={this.props.user.user}>
                 <img src={this.props.user.avatar}/>
                 <div>
-                    <p>我是骚猪pdd,赶紧给我打折</p>
+                    <p>{this.props.user.content}</p>
                     <em></em>
                 </div>
             </div>
